8cb365e835060dd1752daa0b84661954687a28b1
jeeberhardt <qksoneo@gmail.com>
2021-02-17 10:50:45 -0800

    Updated setup and README

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # Meeko
-A new package to prepare small molecules for docking
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![PyPI version fury.io](https://img.shields.io/badge/version-0.1-green.svg)](https://pypi.python.org/pypi/ansicolortags/)
+
+The Python package meeko is a new type of package developped and maintained by the Forli lab also at the [Center for Computational Structural Biology (CCSB)](https://ccsb.scripps.edu). It provides tools covering other docking aspects not handled by the ADFR software suite. This package provides addtionnal tools for the following docking protocols:
+
+* Hydrated docking
+* Macrocycles
+
 
 ## Prerequisites
 
 You need, at a minimum (requirements):
-* Python (>=3.6)
+* Python (>=3.5)
 * Numpy
-* Openbabel
+* Openbabel (>=3)
 
-## Installation
+## Installation (from source)
 
 ```bash
 $ git clone https://github.com/ccsb-scripps/Meeko
@@ -26,7 +34,7 @@ from meeko import obutils
 input_molecule_file = 'example/BACE_macrocycle/BACE_4.mol2'
 mol = obutils.load_molecule_from_file(input_molecule_file)
 
-preparator = MoleculePreparation(merge_hydrogens=True, macrocycle=True, hydrate=True, amide_rigid=True)
+preparator = MoleculePreparation(merge_hydrogens=True, macrocycle=True, hydrate=True)
 preparator.prepare(mol)
 preparator.show_setup()
 
--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ import fnmatch
 from setuptools import setup, find_packages
 
 
+# Path to the directory that contains this setup.py file.
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
 def find_files(directory):
     matches = []
 
@@ -17,32 +20,49 @@ def find_files(directory):
     return matches
 
 
-setup(name="meeko",
-      version=0.1,
-      description="Meeko",
-      author="Stefano Forli",
-      author_email="forli@scripps.edu",
-      url="https://github.com/ccsb-scripps/meeko",
-      packages=find_packages(),
-      scripts=["scripts/mk_prepare_ligand.py"],
-               #"scripts/dry.py",
-               #"scripts/mapwater.py",
-               #"scripts/wet.py"],
-      package_data={
-            "meeko" : ["data/*"]
-      },
-      data_files=[("", ["README.md", "LICENSE"]),
-                  ("scripts", find_files("scripts"))],
-      include_package_data=True,
-      zip_safe=False,
-      license="Apache-2.0",
-      keywords=["molecular modeling", "drug design",
-                "docking", "autodock"],
-      classifiers=["Programming Language :: Python :: 3.6",
-                   "Programming Language :: Python :: 3.7",
-                   "Programming Language :: Python :: 3.8",
-                   "Programming Language :: Python :: 3.9"
-                   "Operating System :: Unix",
-                   "Operating System :: MacOS",
-                   "Topic :: Scientific/Engineering"]
+setup(
+    name="meeko",
+    version='0.1.dev2',
+    author="Stefano Forli",
+    author_email="forli@scripps.edu",
+    url="https://github.com/ccsb-scripps/meeko",
+    description='Python package for preparing small molecule for docking',
+    long_description=open(os.path.join(base_dir, 'README.md')).read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    scripts=["scripts/mk_prepare_ligand.py"],
+           #"scripts/dry.py",
+           #"scripts/mapwater.py",
+           #"scripts/wet.py"],
+    package_data={"meeko" : ["data/*"]},
+    data_files=[("", ["README.md", "LICENSE"]),
+                ("scripts", find_files("scripts"))],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['numpy>=1.18'],
+    python_requires='>=3.5.*',
+    license="Apache-2.0",
+    keywords=["molecular modeling", "drug design",
+            "docking", "autodock"],
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: Other Environment',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        #'Operating System :: Microsoft :: Windows',
+        'Operating System :: OS Independent',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: C++',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Software Development :: Libraries'
+    ]
 )
