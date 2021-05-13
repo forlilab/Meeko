9618e9233f7b23db636e9731addee705d1407e07
jeeberhardt <qksoneo@gmail.com>
2021-05-13 12:26:33 -0700

    converged to a better command line interface

--- scripts/mk_copy_coords.py
+++ scripts/mk_copy_coords.py
@@ -14,50 +14,63 @@ from meeko import obutils
 
 
 def cmd_lineparser():
-    parser = argparse.ArgumentParser(description="Copy coords from PDBQT (or DLG) into original file")
-    parser.add_argument("-i", "--original_input", dest="input_filename", required=True,
-                        action="store", help="input molecule file (e.g. SDF) that was used to prepare the PDBQT")
-    parser.add_argument("-c", "--conformers", dest="coords_filename", required=True,
-                        action="store", help="PDBQT or DLG file to get coordinates from")
-    parser.add_argument("-o", "--output_filename", dest="output_filename",
-                        action="store", help="default is to print to STDOUT. Overrides -w")
-    parser.add_argument("-w", "--outfn_auto", dest="outfn_auto",
-            action="store_true", help="Output filename takes basename and path from --conformers and extension from --original_input. Ignored if -o is specified. WARNING: original input may be overwritten.")
+    parser = argparse.ArgumentParser(description='Copy atom coordinates from PDBQT (or DLG) file \
+                                                  to original molecule file format (SDF or MOL2)')
+    parser.add_argument('-i', '--original_input', dest='input_filename', required=True,
+                        action='store', help='Input molecule file (e.g. SDF or MOL2) that was used to \
+                        prepare the PDBQT filename.')
+    parser.add_argument('-c', '--conformers', dest='coords_filename', required=True,
+                        action='store', help='PDBQT or DLG file to get coordinates from.')
+    parser.add_argument('-o', '--output_filename', dest='output_filename',
+                        action='store', help='Output molecule filename. If not specified, suffix _docked is \
+                        added to the filename based on the input molecule file, and using the same \
+                        molecule file format')
+    parser.add_argument('-s', '--suffix', dest='suffix_name', default='_docked',
+                        action='store', help='Add suffix to output filename if -o/--output_filename \
+                        not specified. WARNING: If specified as empty string (\'\'), this will overwrite \
+                        the original molecule input file (default: _docked).')
+    parser.add_argument('-', '--',  dest='redirect_stdout', action='store_true',
+                        help='do not write file, redirect output to STDOUT. Arguments -o/--output_filename \
+                        is ignored.')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
-
     args = cmd_lineparser()
     input_filename = args.input_filename
     coords_filename = args.coords_filename
     output_filename = args.output_filename
-    outfn_auto = args.outfn_auto
+    suffix_name = args.suffix_name
+    redirect_stdout = args.redirect_stdout
 
-    output_string = ""
+    output_string = ''
 
-    obmol = obutils.load_molecule_from_file(input_filename)
+    ori_obmol = obutils.load_molecule_from_file(input_filename)
+
     is_dlg = coords_filename.endswith('.dlg')
     pdbqt_mol = PDBQTMolecule(coords_filename, is_dlg=is_dlg)
 
-    output_format = 'sdf'
-    if output_filename is not None:
+    if not redirect_stdout and output_filename is not None:
+        # If no output_filename is specified, the format will be the
+        # same as the input molecule format
         output_format = os.path.splitext(output_filename)[1][1:]
+    else:
+        output_format = os.path.splitext(input_filename)[1][1:]
+
     conv = ob.OBConversion()
     success = conv.SetOutFormat(output_format)
     if not success:
-        raise RuntimeError("file format %s not recognized by openbabel" % output_format)
+        raise RuntimeError('Input molecule file format %s not recognized by OpenBabel.' % output_format)
 
     for pose in pdbqt_mol:
-        tmp_obmol = ob.OBMol(obmol) # connectivity may be corrupted by removing and adding Hs multiple times
-        pose.copy_coordinates_to_obmol(tmp_obmol)
-        output_string += conv.WriteString(tmp_obmol)
+        copy_obmol = ob.OBMol(ori_obmol) # connectivity may be corrupted by removing and adding Hs multiple times
+        pose.copy_coordinates_to_obmol(copy_obmol)
+        output_string += conv.WriteString(copy_obmol)
 
-    if (output_filename is None) and (outfn_auto):
-        outfn = os.path.splitext(coords_filename)[0] + os.path.splitext(input_filename)[1]
-        print(output_string, file=open(outfn, 'w'))
-    elif (output_filename is None) and (not outfn_auto):
-        print(output_string)
-    else:
+    if not redirect_stdout:
+        if output_filename is None:
+            output_filename = '%s%s.%s' % (os.path.splitext(coords_filename)[0], suffix_name, output_format)
+
         print(output_string, file=open(output_filename, 'w'))
-
+    else:
+        print(output_string)
