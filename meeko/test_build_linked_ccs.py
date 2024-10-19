096fbebf09ebd50e2e3f627be3738807a994b84e
Amy He <ayhe1768@gmail.com>
2024-10-19 15:15:56 -0700

    drop test file

--- meeko/test_build_linked_ccs.py
+++
@@ -1,5 +0,0 @@
-from meeko.chemtempgen import *
-basename = 'MLZ'
-cc_list = build_linked_CCs(basename)
-for cc in cc_list:
-    fetch_template_dict = json.loads(export_chem_templates_to_json([cc]))['residue_templates'][cc.resname]
