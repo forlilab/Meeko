adef419363b164325495d76978f58dbc92eaaf20
Amy He <ayhe1768@gmail.com>
2025-03-31 12:39:31 -0700

    modify test

--- test/polymer_creation_test.py
+++ test/polymer_creation_test.py
@@ -435,17 +435,18 @@ def test_write_pdbqt_from_pqr():
         mk_prep_for_pqr
     )
     pdbqt_rigid = PDBQTWriterLegacy.write_from_polymer(polymer)[0].split("\n")
-    expected_lines = (
-        "ATOM      1  C   THR     1      43.983  16.642   1.087  1.00  0.00     0.489 C ",
-        "ATOM      2  O   THR     1      44.150  17.855   0.925  1.00  0.00    -0.611 OA",
-        "ATOM      3  CA  THR     1      44.862  15.936   2.105  1.00  0.00     0.293 C ",
-        "ATOM      4  N   THR     1      46.148  16.581   2.104  1.00  0.00    -0.356 N ",
-        "ATOM      5  CB  THR     1      44.293  16.088   3.528  1.00  0.00     0.000 C ",
-        "ATOM      6 CG2  THR     1      43.175  15.110   3.826  1.00  0.00     0.000 C ",
-        "ATOM      7 OG1  THR     1      45.409  15.915   4.403  1.00  0.00    -0.544 OA",
-        "ATOM      8 HG1  THR     1      45.246  15.149   5.034  1.00  0.00     0.436 HD",
-        "ATOM      9  H   THR     1      46.041  17.581   2.102  1.00  0.00     0.293 HD",
-    )
+    expected_lines = """ATOM      1  C   THR     1      43.983  16.642   1.087  1.00  0.00     0.550 C 
+ATOM      2  O   THR     1      44.150  17.855   0.925  1.00  0.00    -0.550 OA
+ATOM      3  CA  THR     1      44.862  15.936   2.105  1.00  0.00     0.330 C 
+ATOM      4  N   THR     1      46.148  16.581   2.104  1.00  0.00    -0.320 N 
+ATOM      5  CB  THR     1      44.293  16.088   3.528  1.00  0.00     0.000 C 
+ATOM      6 CG2  THR     1      43.175  15.110   3.826  1.00  0.00     0.000 C 
+ATOM      7 OG1  THR     1      45.409  15.915   4.403  1.00  0.00    -0.490 OA
+ATOM      8 HG1  THR     1      45.246  15.149   5.034  1.00  0.00     0.490 HD
+ATOM      9  H1  THR     1      46.041  17.581   2.102  1.00  0.00     0.330 HD
+ATOM     10  H2  THR     1      46.674  16.320   2.920  1.00  0.00     0.330 HD
+ATOM     11  H3  THR     1      46.675  16.317   1.289  1.00  0.00     0.330 HD
+""".splitlines()
     for i, line in enumerate(expected_lines): 
         if pdbqt_rigid[i][:len(line)] != line: 
             print(pdbqt_rigid[i][:len(line)])
