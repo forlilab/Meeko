26191037376b345d68d0b1d99f40a0deb49adc38
Amy He <ayhe1768@gmail.com>
2025-04-16 03:47:04 -0700

    add 2nd test to test add_variants

--- test/chemtempgen_test.py
+++ test/chemtempgen_test.py
@@ -1,18 +1,24 @@
 import pathlib
 import json
 import meeko
-from meeko import ResidueChemTemplates
-from meeko.chemtempgen import export_chem_templates_to_json, build_noncovalent_CC, ChemicalComponent
+from meeko.chemtempgen import ChemicalComponent
+from meeko.chemtempgen import export_chem_templates_to_json
+from meeko.chemtempgen import build_noncovalent_CC
+from meeko.chemtempgen import build_linked_CCs
 
 pkgdir = pathlib.Path(meeko.__file__).parents[1]
 default_template_file = pkgdir / "meeko/data/residue_chem_templates.json"
+nakb_template_file = pkgdir / "meeko/data/NAKB_templates.json"
 
-def template_equality_check(basename: str, suffix: str, cc_instance: ChemicalComponent) -> bool:
+def template_equality_check(ref_template_file: str, basename: str, 
+                            suffix: str, cc_instance: ChemicalComponent) -> bool:
     """
     Check if the JSON representation of a residue template matches the default data.
 
     Parameters
     ----------
+    ref_template_file : str
+        The reference template file to compare against
     basename : str
         The residue name to check
     suffix : str
@@ -27,13 +33,16 @@ def template_equality_check(basename: str, suffix: str, cc_instance: ChemicalCom
     """
 
     # Find the expected template from default data file for the given residue name + suffix
-    with open(default_template_file, "r") as f:
-        default_templates = json.load(f)
-    expected = default_templates["residue_templates"][basename + suffix]
+    with open(ref_template_file, "r") as f:
+        ref_templates = json.load(f)
+    expected = ref_templates["residue_templates"][basename + suffix]
 
     # Get the comparable JSON representation of the made residue template in test
     result_json = export_chem_templates_to_json([cc_instance])
     parsed_result = json.loads(result_json)
+
+    print(f"Expected: {expected}")
+    print(f"Parsed: {parsed_result['residue_templates'][basename]}")
 
     return parsed_result["residue_templates"][basename] == expected
 
@@ -44,4 +53,15 @@ def test_build_noncovalent_CC():
     assert cc is not None
     assert isinstance(cc, ChemicalComponent)
 
-    assert template_equality_check(basename, "_fl-ccd", cc)
+    assert template_equality_check(default_template_file, basename, "_fl-ccd", cc)
+
+def test_add_variants():
+    basename = "AMP" 
+    cc_list = build_linked_CCs(basename)
+
+    for cc in cc_list:
+        assert cc is not None
+        assert isinstance(cc, ChemicalComponent)
+
+        assert template_equality_check(default_template_file, cc.resname, "-ccd", cc)
+
