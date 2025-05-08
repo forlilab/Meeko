forlilab/Meeko

commit c3550596a3a2b9003abd279f11eee8499d58beba

remove bond_order from Bond

meeko/bondtyper.py
@@ -56,10 +56,7 @@ class BondTyperLegacy:
 
         for bond_id, bond in setup.bond_info.items():
             rotatable = True
-            bond_order = bond.order
             if bond_id in to_rigidify:
-                rotatable = False
-            if bond_order > 1:
                 rotatable = False
             # check if bond is amide
             if (
@@ -67,6 +64,4 @@ class BondTyperLegacy:
                 or (bond_id[1], bond_id[0]) in amide_bonds
             ) and not flexible_amides:
                 rotatable = False
-                bond_order = 99
             bond.rotatable = rotatable
-            bond.order = bond_order

meeko/molsetup.py
@@ -51,7 +51,6 @@ DEFAULT_ATOM_TYPE = None
 DEFAULT_IS_IGNORE = False
 DEFAULT_GRAPH = []
 
-DEFAULT_BOND_ORDER = 0
 DEFAULT_BOND_ROTATABLE = False
 
 DEFAULT_RING_CORNER_FLIP = False
@@ -282,20 +281,17 @@ class Bond:
     canon_id: (int, int)
     index1: int
     index2: int
-    order: int = DEFAULT_BOND_ORDER
     rotatable: bool = DEFAULT_BOND_ROTATABLE
 
     def __init__(
         self,
         index1: int,
         index2: int,
-        order: int = DEFAULT_BOND_ORDER,
         rotatable: bool = DEFAULT_BOND_ROTATABLE,
     ):
         self.canon_id = self.get_bond_id(index1, index2)
         self.index1 = index1
         self.index2 = index2
-        self.order = order
         self.rotatable = rotatable
         return
 
@@ -342,16 +338,15 @@ class Bond:
             return obj
 
         # Check that all the keys we expect are in the object dictionary as a safety measure
-        expected_json_keys = {"canon_id", "index1", "index2", "order", "rotatable"}
+        expected_json_keys = {"canon_id", "index1", "index2", "rotatable"}
         if set(obj.keys()) != expected_json_keys:
             return obj
 
         # Constructs a bond object from the provided keys.
         index1 = obj["index1"]
         index2 = obj["index2"]
-        order = obj["order"]
         rotatable = obj["rotatable"]
-        output_bond = Bond(index1, index2, order, rotatable)
+        output_bond = Bond(index1, index2, rotatable)
         return output_bond
 
 
@@ -713,7 +708,6 @@ class MoleculeSetup:
         self,
         atom_index_1: int,
         atom_index_2: int,
-        order: int = DEFAULT_BOND_ORDER,
         rotatable: bool = DEFAULT_BOND_ROTATABLE,
     ) -> None:
         """
@@ -725,8 +719,6 @@ class MoleculeSetup:
             Atom index of one of the atoms in the bond
         atom_index_2: int
             Atom index of the other atom in the bond
-        order: int
-            Bond order to set for the bond
         rotatable: bool
             Indicates whether the bond is rotatable
 
@@ -750,7 +742,7 @@ class MoleculeSetup:
         if atom_index_1 not in self.atoms[atom_index_2].graph:
             self.atoms[atom_index_2].graph.append(atom_index_1)
         # Creates new bond object and uses its internal canonical bond id to add it to MoleculeSetup bond tracking.
-        new_bond = Bond(atom_index_1, atom_index_2, order, rotatable)
+        new_bond = Bond(atom_index_1, atom_index_2, rotatable)
         self.bond_info[new_bond.canon_id] = new_bond
         return
 
@@ -1314,7 +1306,7 @@ class MoleculeSetup:
 
         print("\n==============[ BONDS ]================")
         # For sanity users, we won't show those keys for now
-        keys_to_not_show = ["bond_order", "type"]
+        keys_to_not_show = ["type"]
         for bond_id, bond in list(self.bond_info.items()):
             t = ", ".join(
                 "%s: %s" % (i, j)
@@ -1650,15 +1642,8 @@ class RDKitMoleculeSetup(MoleculeSetup, MoleculeSetupExternalToolkit):
         for b in self.mol.GetBonds():
             idx1 = b.GetBeginAtomIdx()
             idx2 = b.GetEndAtomIdx()
-            bond_order = int(b.GetBondType())
-            # fix the RDKit aromatic type (FIXME)
-            if bond_order == 12:  # aromatic
-                bond_order = 5
-            if bond_order == 1:
-                rotatable = True
-            else:
-                rotatable = False
-            self.add_bond(idx1, idx2, order=bond_order, rotatable=rotatable)
+            rotatable = int(b.GetBondType()) == 1
+            self.add_bond(idx1, idx2, rotatable=rotatable)
 
     def find_pattern(self, smarts: str):
         """
@@ -2061,10 +2046,8 @@ class OBMoleculeSetup(MoleculeSetup, MoleculeSetupExternalToolkit):
         for b in ob.OBMolBondIter(self.mol):
             idx1 = b.GetBeginAtomIdx() - 1
             idx2 = b.GetEndAtomIdx() - 1
-            bond_order = b.GetBondOrder()
-            if b.IsAromatic():
-                bond_order = 5
-            self.add_bond(idx1, idx2, order=bond_order)
+            rotatable = b.GetBondOrder() == 1
+            self.add_bond(idx1, idx2, rotatable=rotatable)
 
     def get_mol_name(self):
         """
@@ -2181,7 +2164,6 @@ class BondEncoder(json.JSONEncoder):
                 "canon_id": obj.canon_id,
                 "index1": obj.index1,
                 "index2": obj.index2,
-                "order": obj.order,
                 "rotatable": obj.rotatable,
             }
         return json.JSONEncoder.default(self, obj)

meeko/writer.py
@@ -92,7 +92,7 @@ def oids_json_from_setup(molsetup, name="LigandFromMeeko"):
     elements_line = "elements = %s\n" % (",".join(elements))
 
     bonds = [[] for _ in range(count_oids)]
-    bond_orders = [[] for _ in range(count_oids)]
+    # bond_orders = [[] for _ in range(count_oids)]
     static_links = []
     for i, j in molsetup.bond_info.keys():
         if molsetup.get_is_ignore(i) or molsetup.get_is_ignore(j):
@@ -105,13 +105,13 @@ def oids_json_from_setup(molsetup, name="LigandFromMeeko"):
         oid_i = indexmap[i]
         oid_j = indexmap[j]
         bonds[oid_i].append("%d" % (oid_j + index_start))
-        bond_orders[oid_i].append("%d" % molsetup.bond_info[(i, j)].order)
+        # bond_orders[oid_i].append("%d" % molsetup.bond_info[(i, j)].order)
         if not molsetup.bond_info[(i, j)].rotatable:
             static_links.append("%d,%d" % (oid_i + index_start, oid_j + index_start))
     bonds = [",".join(j_list) for j_list in bonds]
     bonds_line = "connectivity = {%s}\n" % ("|".join(bonds))
-    bond_orders = [",".join(orders) for orders in bond_orders]
-    bondorder_line = "bond_order = {%s}\n" % ("|".join(bond_orders))
+    # bond_orders = [",".join(orders) for orders in bond_orders]
+    # bondorder_line = "bond_order = {%s}\n" % ("|".join(bond_orders))
     staticlinks_line = "static_links = {%s}\n" % ("|".join(static_links))
 
     output = ""
@@ -120,7 +120,7 @@ def oids_json_from_setup(molsetup, name="LigandFromMeeko"):
     output += charges_line
     output += elements_line
     output += bonds_line
-    output += bondorder_line
+    #output += bondorder_line
     output += staticlinks_line
     output += "number = 1\t\t// can only be 1 for the sandbox currently (but any number for classical MC)\n"
     output += "group_dipole = 1\t// not relevant for sandbox but classical MC\n"
@@ -205,7 +205,7 @@ def oids_block_from_setup(molsetup, name="LigandFromMeeko"):
     elements_line = "elements = %s\n" % (",".join(elements))
 
     bonds = [[] for _ in range(count_oids)]
-    bond_orders = [[] for _ in range(count_oids)]
+    # bond_orders = [[] for _ in range(count_oids)]
     static_links = []
     for i, j in molsetup.bond_info.keys():
         if molsetup.get_is_ignore(i) or molsetup.get_is_ignore(j):
@@ -218,13 +218,13 @@ def oids_block_from_setup(molsetup, name="LigandFromMeeko"):
         oid_i = indexmap[i]
         oid_j = indexmap[j]
         bonds[oid_i].append("%d" % (oid_j + index_start))
-        bond_orders[oid_i].append("%d" % molsetup.bond_info[(i, j)].order)
+        # bond_orders[oid_i].append("%d" % molsetup.bond_info[(i, j)].order)
         if not molsetup.bond_info[(i, j)].rotatable:
             static_links.append("%d,%d" % (oid_i + index_start, oid_j + index_start))
     bonds = [",".join(j_list) for j_list in bonds]
     bonds_line = "connectivity = {%s}\n" % ("|".join(bonds))
-    bond_orders = [",".join(orders) for orders in bond_orders]
-    bondorder_line = "bond_order = {%s}\n" % ("|".join(bond_orders))
+    # bond_orders = [",".join(orders) for orders in bond_orders]
+    # bondorder_line = "bond_order = {%s}\n" % ("|".join(bond_orders))
     staticlinks_line = "static_links = {%s}\n" % ("|".join(static_links))
 
     output = ""
@@ -233,7 +233,7 @@ def oids_block_from_setup(molsetup, name="LigandFromMeeko"):
     output += charges_line
     output += elements_line
     output += bonds_line
-    output += bondorder_line
+    # output += bondorder_line
     output += staticlinks_line
     output += "number = 1\t\t// can only be 1 for the sandbox currently (but any number for classical MC)\n"
     output += "group_dipole = 1\t// not relevant for sandbox but classical MC\n"

commit a4cdf5cfe57d646b9c66c6a5cae95b93ef6c63b7

only single bonds are rotatable (mostly)
fixes bug introduced in c3550596a3a2b9003abd279f11eee8499d58beba

meeko/bondtyper.py
@@ -4,6 +4,8 @@
 # Meeko bond typer
 #
 
+from rdkit.Chem.rdchem import BondType
+
 
 class BondTyperLegacy:
 
@@ -63,7 +65,13 @@ class BondTyperLegacy:
                 to_rigidify.add((atom_b, atom_a))
 
         for bond_id, bond in setup.bond_info.items():
-            rotatable = True
+            if (
+                bond_id[0] >= setup.mol.GetNumAtoms()
+                or bond_id[1] >= setup.mol.GetNumAtoms()
+            ):
+                continue  # at least one of the atoms is pseudo or dummy
+            rdkit_bond = setup.mol.GetBondBetweenAtoms(bond_id[0], bond_id[1])
+            rotatable = rdkit_bond.GetBondType() == BondType.SINGLE
             if bond_id in to_rigidify:
                 rotatable = False
             # check if bond is amide

test/flexibility_data/has_alkene.mol
@@ -1,0 +1,57 @@
+
+     RDKit          3D
+
+ 25 26  0  0  0  0  0  0  0  0999 V2000
+   -1.9370  -10.3880   19.3090 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -3.1720  -11.2150   19.0010 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -3.3160  -11.6680   17.8230 O   0  0  0  0  0  0  0  0  0  0  0  0
+   -3.9720  -11.4220   19.9710 O   0  0  0  0  0  0  0  0  0  0  0  0
+   -0.8640  -11.0350   19.9630 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -1.7990   -9.0220   18.9810 C   0  0  0  0  0  0  0  0  0  0  0  0
+    0.3140  -10.3630   20.2840 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -0.6060   -8.3550   19.3300 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -2.8880   -8.2330   18.3700 C   0  0  0  0  0  0  0  0  0  0  0  0
+    0.4440   -9.0190   19.9670 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -3.3830   -7.1140   18.9240 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -4.3750   -7.1060   19.9510 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -4.9030   -8.2100   20.6040 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -5.1440   -5.6680   20.5100 S   0  0  0  0  0  0  0  0  0  0  0  0
+   -5.8970   -7.8760   21.5690 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -6.1200   -6.5210   21.6210 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -0.9614  -12.0987   20.2260 H   0  0  0  0  0  0  0  0  0  0  0  0
+    1.1356  -10.8952   20.7857 H   0  0  0  0  0  0  0  0  0  0  0  0
+   -0.5004   -7.2857   19.0945 H   0  0  0  0  0  0  0  0  0  0  0  0
+   -3.3137   -8.5781   17.4162 H   0  0  0  0  0  0  0  0  0  0  0  0
+    1.3695   -8.4791   20.2161 H   0  0  0  0  0  0  0  0  0  0  0  0
+   -3.0016   -6.1466   18.5653 H   0  0  0  0  0  0  0  0  0  0  0  0
+   -4.5854   -9.2426   20.3971 H   0  0  0  0  0  0  0  0  0  0  0  0
+   -6.4215   -8.6125   22.1955 H   0  0  0  0  0  0  0  0  0  0  0  0
+   -6.8423   -6.0424   22.2987 H   0  0  0  0  0  0  0  0  0  0  0  0
+  1  2  1  0
+  2  3  2  0
+  2  4  1  0
+  5  1  2  0
+  1  6  1  0
+  7  5  1  0
+  6  8  2  0
+  6  9  1  0
+ 10  7  2  0
+  8 10  1  0
+  9 11  2  0
+ 11 12  1  0
+ 12 13  2  0
+ 14 12  1  0
+ 13 15  1  0
+ 16 14  1  0
+ 15 16  2  0
+  5 17  1  0
+  7 18  1  0
+  8 19  1  0
+  9 20  1  0
+ 10 21  1  0
+ 11 22  1  0
+ 13 23  1  0
+ 15 24  1  0
+ 16 25  1  0
+M  CHG  1   4  -1
+M  END

test/flexibility_test.py
@@ -106,3 +106,11 @@ def test_non_sequential_atom_ordering_05():
         expected_rot_bonds={(14, 16), (16, 17), (17, 22), (10, 11), (11, 19)},
         expected_sizes_of_rigid_bodies=[17, 1, 5, 2, 2],
     )
+
+def test_double_bond_is_rigid():
+    run(
+        fn="has_alkene.mol",
+        folder="flexibility_data",
+        expected_rot_bonds={(0, 1), (5, 8), (10, 11)},
+        expected_sizes_of_rigid_bodies=[3, 6, 5, 2],
+    )