41ff6414e53d71c027ccc20d9cbf0f08d2268ba5
diogom <diogom@scripps.edu>
2026-08-27 11:30:37 -0700

    update version strings to 0.8.0

--- meeko/__init__.py
+++ meeko/__init__.py
@@ -4,7 +4,7 @@
 # Meeko
 #
 
-__version__ = "0.7.1"
+__version__ = "0.8.0"
 
 try:
     import prody
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def find_files(directory):
 
 setup(
     name="meeko",
-    version='0.7.1',
+    version='0.8.0',
     author="Forli Lab",
     author_email="forli@scripps.edu",
     url="https://github.com/forlilab/meeko",
